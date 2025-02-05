29c4d67244d942c34d36c0cdfaa4705d77856eeb
shenixo <abdulrazaqolaseni@gmail.com>
2025-02-04 19:18:26 -0800

    second push

--- .history/vite.config_20250204190838.js
+++ .history/vite.config_20250204190838.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Color_match_game/",
+  base: "/color_match_game/",
 })
